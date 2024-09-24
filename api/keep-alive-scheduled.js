6278a9ed05fc0e4d163b0a7025c7c4848ece24af
Anupam Tarai <anutarai.2001@gmail.com>
2024-09-24 16:52:30 +0530

    add health check

---
+++ api/keep-alive-scheduled.js
@@ -0,0 +1,17 @@
+
+export default async function handler(req, res) {
+  const backendUrl = 'https://event-managment-backend-bn7q.onrender.com/health-check'; 
+
+  try {
+      const response = await fetch(backendUrl);
+      if (response.ok) {
+          console.log('Backend is up and running');
+      } else {
+          console.log('Backend might be down');
+      }
+  } catch (error) {
+      console.log('Error pinging backend:', error);
+  }
+
+  res.status(200).json({ status: 'success' });
+}
